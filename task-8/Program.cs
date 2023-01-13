319969ee3b5d6c931d35db79096eb4f42abe0a89
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-13 13:32:09 +0300

    Complete task 8

---
+++ task-8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= N)
+{
+	Console.Write($"{i} ");
+	i += 2;
+}
+Console.WriteLine();
