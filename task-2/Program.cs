c5102f6aae6e3d81269720d1c9ebb01fbac8efda
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-13 11:24:58 +0300

    Complete task 2

---
+++ task-2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Максимальное число: ");
+if (a > b)
+	Console.WriteLine(a);
+else
+	Console.WriteLine(b);
