f7e4d5e41c9e0f8a2f6076142b796773085f0672
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-13 11:47:23 +0300

    Complete task 1

---
+++ task-1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b * b == a)
+	Console.WriteLine("Да");
+else
+	Console.WriteLine("Нет");
