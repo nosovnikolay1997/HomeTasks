45b28321dc042399782025b5aa6f143d479d30e9
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-18 00:02:27 +0300

    Complete task 15

---
+++ task-15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число от 1 до 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+	Console.WriteLine("Ошибка!\nВведите число от 1 до 7: ");
+	n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 6 || n == 7)
+	Console.WriteLine("Да");
+else
+	Console.WriteLine("Нет");
