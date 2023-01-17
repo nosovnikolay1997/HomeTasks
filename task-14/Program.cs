7e7bc4df94d337564a1ffa634ce41e0f4400908d
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-17 11:05:59 +0300

    Complete task 14

---
+++ task-14/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if ((n % 7 == 0) && (n % 23 == 0))
+	Console.WriteLine("Да");
+else
+	Console.WriteLine("Нет");
