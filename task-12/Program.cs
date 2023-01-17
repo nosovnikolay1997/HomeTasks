425843f3c37b738784887fa98a92b279a300dffd
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-17 10:50:11 +0300

    Complete task 12

---
+++ task-12/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int rem = a % b;
+if (rem == 0)
+	Console.WriteLine($"{b} кратно {a}");
+else
+	Console.WriteLine($"{b} не кратно {a}, остаток {rem}");
