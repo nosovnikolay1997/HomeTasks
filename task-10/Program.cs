837c3b48e676367e2dc780da5e0fd580cadf795c
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-17 11:18:24 +0300

    Complete task 10

---
+++ task-10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 999)
+{
+	Console.Write("Ошибка!\nВведите трёхзначное число: ");
+	n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Вторая цифра: {(n / 10) % 10}");
