6a53bbb8cfde6238b883aa6d4df5b00d33f45b8f
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-17 23:52:46 +0300

    Complete task 13

---
+++ task-13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+	Console.WriteLine("Третьей цифры нет.");
+else
+{
+	while (n > 999)
+		n = n / 10;
+	Console.WriteLine($"{n % 10}");
+}
