bca8902e3a74b75b5aaf5963eec93aad1fb1464b
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-20 10:44:26 +0300

    Change WriteLine to Write in task 22

--- task-22/Program.cs
+++ task-22/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите целое положительное число: ");
+Console.Write("Введите целое положительное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 while (n < 1)
 {
-	Console.WriteLine("Ошибка\nВведите целое положительное число: ");
+	Console.Write("Ошибка\nВведите целое положительное число: ");
 	n = Convert.ToInt32(Console.ReadLine());
 }
 for (int i = 1; i < n; i++)
