e354dce476a7d1945f192ea2db6ebff293e31396
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-20 10:46:28 +0300

    Complete task 23

---
+++ task-23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите целое положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1)
+{
+	Console.Write("Ошибка\nВведите целое положительное число: ");
+	n = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 1; i < n; i++)
+	Console.Write($"{i*i*i}, ");
+Console.WriteLine($"{n*n*n}");
