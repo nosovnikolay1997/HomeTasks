ab997029e5ee33c1640141a885039c6e3129369a
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-20 11:19:05 +0300

    Complete task 19

---
+++ task-19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите целое неотрицательное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 0)
+{
+	Console.Write("Ошибка!\nВведите целое неотрицательное число: ");
+	n = Convert.ToInt32(Console.ReadLine());
+}
+int n_flipped = 0;
+int m = n;
+while (m > 0)
+{
+	n_flipped = n_flipped*10 + m%10;
+	m = m / 10;
+}
+if (n_flipped == n)
+	Console.WriteLine("Да.");
+else
+	Console.WriteLine("Нет.");
