03243cf0218c6092d696dfec0d420c0e7cbaf184
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-24 10:04:56 +0300

    Complete task 24

---
+++ task-24/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите положительное целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= n; i++)
+	sum += i;
+Console.WriteLine($"Сумма чисел от 1 до {n} = {sum}");
