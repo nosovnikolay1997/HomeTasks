58cd86123848dcbe263d353974f3bcb220995a9f
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-24 10:13:06 +0300

    Complete task 26

---
+++ task-26/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i;
+for (i = 0; n != 0; i++)
+	n = n / 10;
+Console.WriteLine($"Количество цифр: {i}");
