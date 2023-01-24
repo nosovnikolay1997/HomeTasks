0fdfefac27c6cc9f50385a45bbfe345b69a11b5a
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-24 11:22:05 +0300

    Complete task 30

---
+++ task-30/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+	array[i] = rand.Next(0, 2);
+Console.WriteLine($"[{String.Join(", ", array)}]");
