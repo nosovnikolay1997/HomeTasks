ca0597dca23159be594a87834fce0d3240a334d0
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-24 11:10:59 +0300

    Complete task 24 without a loop

--- task-24/Program.cs
+++ task-24/Program.cs
@@ -1,7 +1,4 @@
 ﻿Console.Clear();
 Console.Write("Введите положительное целое число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-for (int i = 1; i <= n; i++)
-	sum += i;
-Console.WriteLine($"Сумма чисел от 1 до {n} = {sum}");
+Console.WriteLine($"Сумма чисел от 1 до {n} = {((1+n)*n)/2}");
