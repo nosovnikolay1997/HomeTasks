242c82103bb0175bebb3351a5c590b25babf73fa
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-24 10:18:21 +0300

    Complete task 28

---
+++ task-28/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+for (int i = 2; i <= n; i++)
+	res = res * i;
+Console.WriteLine($"Произведение чисел от 1 до {n}: {res}");
