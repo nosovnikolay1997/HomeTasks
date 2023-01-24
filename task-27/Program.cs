ec90f2d276e73b846ad7c2fb9597e7d2162c4a93
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-24 11:32:03 +0300

    Complete task 27

---
+++ task-27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите неотрицательное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (n > 0)
+{
+	sum += n % 10;
+	n = n / 10;
+}
+Console.WriteLine($"Сумма цифр: {sum}");
