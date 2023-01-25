5cbef63d51a01257ee4713d9e6efa13f24d23102
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-25 17:02:10 +0300

    Complete task 25

---
+++ task-25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите основание степени: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите показатель степени: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+for (int i = 0; i < b; i++)
+	res = res * a;
+Console.WriteLine($"{a}^{b} = {res}");
