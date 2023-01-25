5f488c91693a1a529d046121591212bbeaa3860b
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-25 17:11:29 +0300

    Complete task 29

---
+++ task-29/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int[] array = new int[8];
+Console.WriteLine($"Введите {array.Length} чисел, каждое с новой строки:");
+for (int i = 0; i < array.Length; i++)
+	array[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"[{String.Join(", ", array)}]");
