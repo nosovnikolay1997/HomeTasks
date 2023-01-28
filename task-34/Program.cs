91c4063475e93493d8feb92f993d550e2d4cdfc9
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-28 17:36:58 +0300

    Complete task 34

---
+++ task-34/Program.cs
@@ -0,0 +1,28 @@
+﻿void InputArray(int[] array, int from, int to)
+{
+	Random rand = new Random();
+	for (int i = 0; i < array.Length; i++)
+		array[i] = rand.Next(from, to);
+}
+
+void PrintArray(int[] array)
+{
+	Console.WriteLine($"[{String.Join(", ", array)}]");
+}
+
+int ReleaseArray(int[] array)
+{
+	int result = 0;
+	foreach (int element in array)
+		if (element % 2 == 0)
+			result++;
+	return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array, 100, 1000);
+PrintArray(array);
+Console.WriteLine($"Количество чётных элементов: {ReleaseArray(array)}");
