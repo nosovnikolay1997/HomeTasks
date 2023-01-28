27258f96d6cebbdb632ded7a543e1e7c5db0e636
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-28 17:48:23 +0300

    Complete task 38

---
+++ task-38/Program.cs
@@ -0,0 +1,33 @@
+﻿void InputArray(double[] array, int from, int to)
+{
+	Random rand = new Random();
+	for (int i = 0; i < array.Length; i++)
+		array[i] = ((double)rand.Next(from*100, to*100))/100.0;
+}
+
+void PrintArray(double[] array)
+{
+	Console.WriteLine($"[{String.Join(", ", array)}]");
+}
+
+double ReleaseArray(double[] array)
+{
+	double min = array[0];
+	double max = array[0];
+	foreach (double element in array)
+	{
+		if (element < min)
+			min = element;
+		if (element > max)
+			max = element;
+	}
+	return max-min;
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array, 0, 100);
+PrintArray(array);
+Console.WriteLine($"Разница между максимальным и минимальным элементом: {ReleaseArray(array)}");
