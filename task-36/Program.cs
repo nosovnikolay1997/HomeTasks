c54e2567e96daa9f8f2da69f5e28b8d3f3495e79
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-01-28 17:41:14 +0300

    Complete task 36

---
+++ task-36/Program.cs
@@ -0,0 +1,27 @@
+﻿void InputArray(int[] array, int from, int to)
+{
+	Random rand = new Random();
+	for (int i = 0; i < array.Length; i++)
+		array[i] = rand.Next(from, to);
+}
+
+void PrintArray(int[] array)
+{
+	Console.WriteLine($"[{String.Join(", ", array)}]");
+}
+
+int ReleaseArray(int[] array)
+{
+	int result = 0;
+	for (int i = 1; i < array.Length; i += 2)
+		result += array[i];
+	return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array, -100, 101);
+PrintArray(array);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {ReleaseArray(array)}");
