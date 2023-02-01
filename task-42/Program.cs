588ad577463fc0dceae8902dba9775b2e176b4e5
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-01 03:47:29 +0300

    Complete task 42

---
+++ task-42/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] ToBinary(int x)
+{
+	//65 should be enough to store 64 bit number
+	int n = 65;
+	int[] binary = new int[n];
+	for (int i = binary.Length - 1; x > 0; i--)
+	{
+		binary[i] = x % 2;
+		x = x / 2;
+	}
+	return binary;
+}
+
+void PrintBinary(int[] binary)
+{
+	//Remove leading zeroes
+	int start;
+	for (start = 0; start < binary.Length - 1; start++)
+		if (binary[start] == 1)
+			break;
+	for (int i = start; i < binary.Length; i++)
+		Console.Write($"{binary[i]}");
+	Console.WriteLine();
+}
+
+Console.Clear();
+Console.WriteLine("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+PrintBinary(ToBinary(a));
