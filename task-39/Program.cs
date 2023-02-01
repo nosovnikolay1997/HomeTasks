6235f2acf7c69340b3e4dc97f0b44564d639fe0c
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-01 03:13:47 +0300

    Complete task 39

---
+++ task-39/Program.cs
@@ -0,0 +1,30 @@
+﻿void InputArray(int[] array, int from, int to)
+{
+	Random rand = new Random();
+	for (int i = 0; i < array.Length; i++)
+		array[i] = rand.Next(from, to);
+}
+
+void PrintArray(int[] array)
+{
+	Console.WriteLine($"[{String.Join(", ", array)}]");
+}
+
+void FlipArray(int[] array)
+{
+	for (int i = 0; i < array.Length / 2; i++)
+	{
+		int buf = array[i];
+		array[i] = array[array.Length - 1 - i];
+		array[array.Length - 1 - i] = buf;
+	}
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array, 0, 100);
+PrintArray(array);
+FlipArray(array);
+PrintArray(array);
