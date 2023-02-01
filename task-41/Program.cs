572480c29b020ea2d77ca19b08c60ac3ee01cc07
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-01 05:10:41 +0300

    Complete task 41

---
+++ task-41/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите количество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите {n} чисел, каждое с новой строки:");
+int count;
+for (count = 0; n > 0; n--)
+	if (Convert.ToInt32(Console.ReadLine()) > 0)
+		count++;
+Console.WriteLine($"Чисел больше 0: {count}");
