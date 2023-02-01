6fe2656ae876802dcbf1bacae641a24997ed5e69
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-01 04:02:48 +0300

    Complete task 44

---
+++ task-44/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите номер числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int previous = 0;
+for (int current = 1; n > 0; n--)
+{
+	Console.Write($"{previous} ");
+	int next = previous + current;
+	previous = current;
+	current = next;
+}
+Console.WriteLine();
