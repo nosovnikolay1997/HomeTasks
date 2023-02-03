31ef877bdf46e4cae096990119c1c8d9343528e0
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-03 10:10:08 +0300

    Complete task 48

---
+++ task-48/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArray(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+		for (int j = 0; j < array.GetLength(1); j++)
+			array[i,j] = i+j;
+}
+
+void PrintArray(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+			Console.Write($"{array[i,j]} ");
+		Console.WriteLine();
+	}
+}
+
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m,n];
+FillArray(array);
+PrintArray(array);
