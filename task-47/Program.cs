6c9c1821368b751854347288e853df838d6d5f80
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-03 10:48:26 +0300

    Complete task 47

---
+++ task-47/Program.cs
@@ -0,0 +1,24 @@
+﻿void FillArray(double[,] array, double min, double max)
+{
+	Random rand = new Random();
+	for (int i = 0; i < array.GetLength(0); i++)
+		for (int j = 0; j < array.GetLength(1); j++)
+			array[i,j] = Math.Round(rand.NextDouble() * (max - min) + min, 2);
+}
+
+void PrintArray(double[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+			Console.Write($"{array[i,j]} ");
+		Console.WriteLine();
+	}
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+double[,] array = new double[input[0], input[1]];
+FillArray(array, 1.0, 10.0);
+PrintArray(array);
