640ca334972cb8b9723e5d61b42b78328c6a34df
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-03 10:40:17 +0300

    Complete task 51

---
+++ task-51/Program.cs
@@ -0,0 +1,33 @@
+﻿void FillArray(int[,] array)
+{
+	Random rand = new Random();
+	for (int i = 0; i < array.GetLength(0); i++)
+		for (int j = 0; j < array.GetLength(1); j++)
+			array[i,j] = rand.Next(-100,101);
+}
+
+void PrintArray(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+			Console.Write($"{array[i,j]} ");
+		Console.WriteLine();
+	}
+}
+
+int SumDiagonal(int[,] array)
+{
+	int result = 0;
+	for (int i = 0; i < array.GetLength(0) && i < array.GetLength(1); i++)
+			result += array[i, i];
+	return result;
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] array = new int[input[0], input[1]];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"\nСумма элементов на диагонали: {SumDiagonal(array)}");
