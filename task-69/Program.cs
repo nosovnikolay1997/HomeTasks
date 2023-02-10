e1f92a087ab255a0c311d0bd4bd66f385991adeb
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-10 10:17:52 +0300

    Complete task 69

---
+++ task-69/Program.cs
@@ -0,0 +1,11 @@
+﻿int Power(int a, int b)
+{
+	if (b == 0)
+		return 1;
+	return a * Power(a, b - 1);
+}
+
+Console.Clear();
+Console.Write("Введите два натуральных числа: ");
+int[] input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+Console.WriteLine($"{input[0]}^{input[1]}={Power(input[0], input[1])}");
