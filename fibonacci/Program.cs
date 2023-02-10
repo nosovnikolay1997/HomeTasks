ee53db6912e645e64deddf3817bb44bff583f436
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-10 10:48:25 +0300

    Complete task about Fibonacci numbers

---
+++ fibonacci/Program.cs
@@ -0,0 +1,10 @@
+﻿int fib(int n)
+{
+	if (n < 2)
+		return n;
+	return fib(n - 1) + fib(n - 2);
+}
+
+Console.Clear();
+Console.Write("Введите неотрицательное число: ");
+Console.WriteLine($"{fib(Convert.ToInt32(Console.ReadLine()))}");
