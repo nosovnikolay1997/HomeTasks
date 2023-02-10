505e59e7c05ce8c9f4db089b290a2b2c99ce85e3
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-10 10:06:51 +0300

    Complete task 65

---
+++ task-65/Program.cs
@@ -0,0 +1,15 @@
+﻿void PrintSequence(int from, int to)
+{
+	if (to > from)
+	{
+		PrintSequence(from, to-1);
+		Console.Write(", ");
+	}
+	Console.Write($"{to}");
+}
+
+Console.Clear();
+Console.Write("Введите два натуральных числа: ");
+int[] input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+PrintSequence(input[0], input[1]);
+Console.WriteLine();
