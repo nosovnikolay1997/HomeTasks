bdbb985e995afa507b417d13f2c7c8ec1d5059d7
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-10 10:13:52 +0300

    Complete task 67

---
+++ task-67/Program.cs
@@ -0,0 +1,11 @@
+﻿int SumOfDigits(int n)
+{
+	if (n == 0)
+		return 0;
+	return SumOfDigits(n / 10) + n % 10;
+}
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр: {SumOfDigits(n)}");
