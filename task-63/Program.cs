5cf5fca3a1bb8f62e30f0ff7d7d38baedee44b41
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-10 10:03:33 +0300

    Complete task 63

---
+++ task-63/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintSequence(int n)
+{
+	if (n > 1)
+	{
+		PrintSequence(n - 1);
+		Console.Write(", ");
+	}
+	Console.Write($"{n}");
+}
+
+Console.Clear();
+Console.Write("Введите натуральное число: ");
+PrintSequence(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine();
