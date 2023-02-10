acc59c20310d418d42abe58221a33e39d371da68
Nikolay Nosov <nosovnikolay1997@gmail.com>
2023-02-10 10:22:07 +0300

    Complete task 66

---
+++ task-66/Program.cs
@@ -0,0 +1,10 @@
+﻿int Sum(int m, int n)
+{
+	if (m == n)
+		return m;
+	return n + Sum(m, n-1);
+}
+Console.Clear();
+Console.Write("Введите два натуральных числа: ");
+int[] input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+Console.WriteLine($"Сумма натуральных элементов от {input[0]} до {input[1]}: {Sum(input[0], input[1])}");
